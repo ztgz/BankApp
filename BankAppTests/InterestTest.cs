f99a13674459ce5fb1de00f96e81791afe790b3b
Arvid Zetterberg <zetterberg.arvid@gmail.com>
2017-10-07 22:10:26 +0200

    save_changes

---
+++ BankAppTests/InterestTest.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using BankApp;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankAppTests
+{
+    [TestClass]
+    public class InterestTest
+    {
+        [TestMethod]
+        public void Daily_Saving_Interest()
+        {
+            Account account = new Account(0,0,10_000);
+
+            account.SetSavingInterest(10);
+
+            account.AddDailyInterest();
+
+            //Numbers from excel, (might have rounding errors)
+            decimal expectedBalance = 10002.6115787607m;
+
+            Assert.AreEqual(expectedBalance, account.Balance);
+        }
+
+        [TestMethod]
+        public void Daily_Saving_Intrest_Negative_Account()
+        {
+            Account account = new Account(0, 0, -10_000);
+
+            account.SetSavingInterest(10);
+
+            account.AddDailyInterest();
+
+            //Numbers from excel, (might have rounding errors)
+            decimal expectedBalance = -10_000;
+
+            Assert.AreEqual(expectedBalance, account.Balance);
+        }
+
+        [TestMethod]
+        public void Daily_Debt_Interest()
+        {
+            Account account = new Account(0, 0, -10_000);
+
+            account.SetDebtInterest(10);
+
+            account.AddDailyInterest();
+
+            //Numbers from excel, (might have rounding errors)
+            decimal expectedBalance = -10002.6115787607m;
+
+            Assert.AreEqual(expectedBalance, account.Balance);
+        }
+
+        [TestMethod]
+        public void Daily_Debt_Interest_Positive_Balance()
+        {
+            Account account = new Account(0, 0, 10_000);
+
+            account.SetDebtInterest(10);
+
+            account.AddDailyInterest();
+
+            //Numbers from excel, (might have rounding errors)
+            decimal expectedBalance = 10000;
+
+            Assert.AreEqual(expectedBalance, account.Balance);
+        }
+    }
+}
